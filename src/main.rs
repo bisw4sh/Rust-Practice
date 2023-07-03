f62cfc19b69fe354f0d7aac07571fcf4ff153736
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-07-03 14:06:43 +0545

    .

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,12 @@ fn main() {
     // You can manipulate expressions more easily with block comments
     // than with line comments. Try deleting the comment delimiters
     // to change the result:
+    
     let x: i32 = 5 + /* 90 + */ 5;
     println!("Is `x` 10 or 100? x = {}", x);
     let name:&str = "Biswash";
     _01_greeting::greeting(name);
+
     //To get sum of two numbers
     println!("enter a number:");
     let mut stra = String::new();
@@ -50,4 +52,3 @@ fn main() {
   
     _02_sum::sum(a,b);
 }
-
